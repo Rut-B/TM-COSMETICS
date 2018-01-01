f4c72ce26c200291bd641c609760577ea8d37a82
Rut Biton <32880198+Rut-B@users.noreply.github.com>
2018-01-01 19:01:13 +0200

    before pull tamar

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,39 +5,59 @@ import { CalendarEvent, } from "angular-calendar";
 import { Component } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFirestoreDocument,AngularFirestoreCollection} from 'angularfire2/firestore';
-import * as user_details from './globals';
+import {Router} from "@angular/router";
 import * as firebase from 'firebase/app';
+import * as navs from './nav.service';
 
+
+export class USER {
+  first_name: string;
+  last_name: string;
+  email: string;
+  address:string;
+  phone:number;
+  is_customer:boolean;
+}
+
+export var isLogin:boolean = false;
+export var corrent_user:USER;
 
 @Injectable()
-
 export class AuthService {
-  public first_name;
-  public last_name;
-  public  email:string;
- 
-
   private users_list:AngularFirestoreCollection<any>;
-  
-
   private _user;
-  private user_colc:AngularFirestoreCollection<any>;
   private users_details:USER[];
-  
-  constructor(public afAuth: AngularFireAuth,public afs:AngularFirestore) { 
 
+  constructor(public afAuth: AngularFireAuth,public afs:AngularFirestore, public router: Router) {  
     this.users_list=this.afs.collection("USERS"); 
-    alert( this.users_list);
     this.users_list.valueChanges().subscribe(res=>{
       this.users_details=res;
-      alert(res);
     });
   }
 
-  public exist_user(email:string,name:string){
-    return true;
-    
+   public exist_user(email:string,first_name:string,last_name:string)
+  {
+
+    for(var i=0;i<this.users_details.length;i++)
+    {
+        if((this.users_details[i].email==email)&&(this.users_details[i].first_name==first_name)&&(this.users_details[i].last_name==last_name))
+        {
+         //if exist create public user
+         //? how do new corrent_user?
+          corrent_user = new USER();
+          corrent_user.address=this.users_details[i].address;
+          corrent_user.first_name=this.users_details[i].first_name;
+          corrent_user.last_name=this.users_details[i].last_name;
+          corrent_user.email=this.users_details[i].email;
+          corrent_user.is_customer=this.users_details[i].is_customer;
+          corrent_user.phone=this.users_details[i].phone;
+          
+          return true;
+        }
+
     }
+    return false;
+  }
 
 
   loginWithGoogle() {
@@ -47,27 +67,24 @@ export class AuthService {
     var verify= user.additionalUserInfo.profile.verified_email;
 
     if(verify){
-    this.first_name= user.additionalUserInfo.profile.first_name;
-    this.last_name = user.additionalUserInfo.profile.given_name;
-    this.email = user.additionalUserInfo.profile.mail;
-    
-    //check what the status:
-    
-        }
-        else{
+      
+    var first_name= user.additionalUserInfo.profile.given_name;
+    var last_name = user.additionalUserInfo.profile.family_name;
+    var email = user.additionalUserInfo.profile.email;
+    alert("in login"+first_name+last_name+email);
+    if(!this.exist_user(email,first_name,last_name))
+    {
 
-        }
-       /* if(exist_user(name,mail)){
-          //check if exist:
-         
-        }
-        
-        else{//join to service
-
-        }*/
-       // this._user = user.user;
-       // console.log(user.user.additionalUserInfo.profile.verified_email);
-        
+    
+    //add new user.
+    //this.router.navigate(["add_customer"]);
+    }
+    else{
+    isLogin= true;
+    navs.viewNavs();
+    }
+  
+      }  
       });
   }
 
@@ -101,11 +118,4 @@ export class AuthService {
 
 
 
-export interface USER {
-  first_name: string;
-  last_name: string;
-  email: string;
-  address:string;
-  phone:number;
-  is_cosmetic:boolean;
-}
+
--- src/app/nav.service.ts
+++ src/app/nav.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import * as users from './auth.service';
 import * as myGlobals from './globals';
 @Injectable()
-export class NavService {
+export class NavService 
+{
     public navItems: NavItem[];
   
-constructor() {
-  if (myGlobals.customer){
+constructor() 
+{
+//alert("in con of nav");
+ 
+// while(!users.isLogin);
+ var exist=true;
+  if (/*users.AuthService.prototype.corrent_user.is_customer*/exist)
+  {
     this.navItems=[
       new NavItem("settings","settings",'/assets/6.png'),
       new NavItem("information","information",'/assets/2.png'),
@@ -15,7 +23,39 @@ constructor() {
       new NavItem("instagram","instagram",'/assets/ins.png'),
     ]
   }
-  else{
+  else
+  {
+    this.navItems=[
+      new NavItem("settings","cosmetician-settings",'/assets/6.png'),
+      new NavItem("store","cosmetician-products",'/assets/3.png'),
+      new NavItem("calendar","cosmetician-calendar",'/assets/4.png')
+    ]
+  }
+ }
+}
+  export class NavItem
+  {
+    constructor(public text:string, public url:string, public image:string)
+    {
+    }
+  
+  }
+export function viewNavs(){
+ 
+  var exist=true;
+  if (/*users.AuthService.prototype.corrent_user.is_customer*/exist)
+  {
+    this.navItems=[
+      new NavItem("settings","settings",'/assets/6.png'),
+      new NavItem("information","information",'/assets/2.png'),
+      new NavItem("store","store",'/assets/3.png'),
+      new NavItem("calendar","calendar",'/assets/4.png'),
+      new NavItem("profile","profile",'/assets/5.png'),
+      new NavItem("instagram","instagram",'/assets/ins.png'),
+    ]
+  }
+  else
+  {
     this.navItems=[
       new NavItem("settings","cosmetician-settings",'/assets/6.png'),
       new NavItem("store","cosmetician-products",'/assets/3.png'),
@@ -23,10 +63,4 @@ constructor() {
     ]
   }
 
-   }
 }
-  export class NavItem{
-    constructor(public text:string, public url:string, public image:string){
-    }
-  
-  }
