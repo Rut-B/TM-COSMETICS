0bddbd5e2c9953cc5add5e84ced4e8277ad70026
chemda <chemda96@gmail.com>
2018-01-02 18:10:29 +0200

    graphic design

--- src/app/cosmetician-settings/cosmetician-settings.component.ts
+++ src/app/cosmetician-settings/cosmetician-settings.component.ts
@@ -2,13 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase';
+import {DatabaseFirebaseService} from '../database-firebase.service'
+
 @Component({
   selector: 'app-cosmetician-settings',
   templateUrl: './cosmetician-settings.component.html',
   styleUrls: ['./cosmetician-settings.component.scss']
 })
 export class CosmeticianSettingsComponent implements OnInit {
-  ngOnInit() {
-  }
+
+
+
+  
+  
+  constructor(public databaseFirebase: DatabaseFirebaseService ) {   
+    
+    
+    }
+    
+    
+      ngOnInit() {
+      }
+     public addProducts(){
+      this.databaseFirebase.addProducts();
+     }
+     public addTreatment(){
+      this.databaseFirebase.addTreatment();
+     }
+     public addLocation(){
+      this.databaseFirebase.addLocation();
+     }
+     public addCustomer()
+    {
+      this.databaseFirebase.addCustomer();  
+    }
+    public addCosmetician(){
+      this.databaseFirebase.addCosmetician();
+    }
+    public addMessageManager(){
+      this.databaseFirebase.addMessageManager();
+    }
+    public addAppointment(){
+      this.databaseFirebase.addAppointment();
+    }
+
 
 }
