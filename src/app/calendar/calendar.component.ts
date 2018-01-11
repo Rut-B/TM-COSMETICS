eadb0937de42e60af7b954234840af5076da3bfa
noamip <noamijofen@gmail.com>
2018-01-11 17:47:01 +0200

    days work!!

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -122,28 +122,83 @@ var today = mm+'/'+dd+'/'+yyyy;
 return today;
 }
 
-trying(){
-  var day=this.events[0].start.getDate().toString();
-  var month=this.events[0].start.getMonth();
-  var year=this.events[0].start.getFullYear();
-  alert(day+", "+month+" ,"+year);
-if(this.viewDate.getMonth()==month&&this.viewDate.getFullYear()==year){
-   var cc=document.getElementsByClassName("cal-cell");
-  for(var i=7;i<42;i++){ 
-   if(cc[i].getElementsByTagName("span")[1]!=null){
-     if(cc[i].getElementsByTagName("span")[1].innerText==day){
-     alert("hay!!!"+" "+cc[i].getElementsByTagName("span")[1].innerText);
-    cc[i].className="a";
+public iterate(){  
+  var cc=document.getElementsByClassName("cal-cell");
+  var cmonth=this.viewDate.getMonth();
+  var cyear=this.viewDate.getFullYear();
+  var daysInMonth=new Date(cyear, cmonth, 0).getDate();
+  for(var i=7;i<38;i++){
+    if(cc[i].getElementsByTagName("span")[1]!=null){
+      if(cc[i].getElementsByTagName("span")[1].innerText=="1"){
+    break;
+    }
+  }
+    else if (cc[i].getElementsByTagName("span")[0].innerText=="1"){
+    break;
+    }
+  }
+
+  var dontDays=[];
+  var t=i;
+  var l=0;
+for(i;i<=t+daysInMonth;i++){ 
+   for(var j=0;j<this.mySpecDays.length;j++){
+     var curr=new Date(this.mySpecDays[j].date);
+     if(curr.getFullYear()==cyear&&curr.getMonth()==cmonth){
+       var cday=curr.getDate().toString(); 
+      if(cc[i].getElementsByTagName("span")[1]!=null){
+        if(cc[i].getElementsByTagName("span")[1].innerText==cday){
+      //  alert("hay!!!"+" "+cc[i].getElementsByTagName("span")[1].innerText);
+        dontDays[l]=cday;
+        l++;
+       cc[i].className="a";
+      }
+     }
+     else{
+       if(cc[i].getElementsByTagName("span")[0].innerText==cday){
+       // alert("hay!!!"+" "+cc[i].getElementsByTagName("span")[0].innerText);
+        dontDays[l]=cday;
+        l++;
+        cc[i].className="a";
+     }   
+    }
    }
+   }//end for mySpecDays 
+   if(this.mySpecDays.length==dontDays.length){
+     break;
+   }
+ }
+ var cc=document.getElementsByClassName("cal-cell");
+ var n; 
+ var s=[];
+ n=0;
+ var m;
+ for( i=0,m=0;m<daysInMonth-dontDays.length;m++,i++){
+   if(cc[(i+t)].getElementsByTagName("span")[1]!=null){
+    n=cc[(i+t)].getElementsByTagName("span")[1].innerText;
   }
   else{
-    if(cc[i].getElementsByTagName("span")[0].innerText==day){
-      alert("hay!!!"+" "+cc[i].getElementsByTagName("span")[0].innerText);
-     cc[i].className="a";
-  }
+   n=cc[(i+t)].getElementsByTagName("span")[0].innerText;
+  }  
+  s.push(n);
+ // s.push(i+" "+n+"bb ");
+  var d=new Date(cyear,cmonth,n); 
+  for(var k=0;k<this.myDays.length;k++){ 
+    //s.push(i+" "+k+" "+n);  
+    var now=this.myDays[k].date;  
+   if(this.check(d.getDay())==now){
+    //console.log(this.check(d.getDay())+","+now+" "+i);
+    cc[(i+t)].className="a2";
+    i--;
+     break;
+   // alert(this.check(d.getDay())+" "+i);
+   }
+  }//end k
+}//end cc
 }
-}
-}
+ public check(i){
+  var arr=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  return(arr[i]);
 }
 public scheduleTime(day:Date, duration:number):boolean{
 return false;
