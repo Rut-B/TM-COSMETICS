e3a26ab9dcaddecba75d4e383eb17d7e9959fc42
chemda <chemda96@gmail.com>
2018-02-25 13:00:41 +0200

    commit

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -30,7 +30,7 @@ export interface aDay{
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent implements OnInit {
   view: string = 'month'; 
